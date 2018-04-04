8c4fc4c40687498762ee089cd24e5ee23e6adfa2
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-04 00:48:58 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-   resources :students, only: [:index, :new, :create, :show]
+   resources :students, only: [:index, :new, :create, :show] #can show go here?
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
